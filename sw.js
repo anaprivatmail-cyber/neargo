0672dfe30572180d6b090aeb110d6c65a048f156
anaprivatmail-cyber <anaprivatmail@gmail.com>
2025-09-18 23:34:43 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
 self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => self.clients.claim());
-self.addEventListener('fetch', () => { /* passthrough */ });
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+// Passthrough (network)
+self.addEventListener('fetch', () => { /* no-op */ });

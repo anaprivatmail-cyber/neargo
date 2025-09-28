16e611595a170e8ba553a1708e6eeffd16cb2111
anaprivatmail-cyber <anaprivatmail@gmail.com>
2025-09-29 01:31:46 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,16 @@
+// version: 2025-09-29-1
+
+self.addEventListener('install', () => self.skipWaiting());
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+// Passthrough (network)
+self.addEventListener('fetch', () => { /* no-op */ });
 self.addEventListener('install', () => self.skipWaiting());
 
 self.addEventListener('activate', (e) => {

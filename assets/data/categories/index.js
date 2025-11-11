d55b7b1271f8275af4aba7c5ef434eeb89db040a
anaprivatmail-cyber <anaprivatmail-cyber@users.noreply.github.com>
2025-11-11 10:19:03 +0100

    chore(categories): cache-bust taxonomy source import to refresh categories for clients

--- assets/data/categories/index.js
+++ assets/data/categories/index.js
@@ -1,4 +1,4 @@
-import { CATEGORY_SOURCE } from './source.js';
+import { CATEGORY_SOURCE } from './source.js?v=20251111';
 
 export const EVENT_CATEGORY_SOURCE = CATEGORY_SOURCE.events;
 export const SERVICE_CATEGORY_SOURCE = CATEGORY_SOURCE.services;
